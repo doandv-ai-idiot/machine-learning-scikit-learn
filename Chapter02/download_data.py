f88954e577247b158c3a3d9ccca899ec4771ffbd
doandv-ai-idiot <doandv.python.dev@gmail.com>
2021-12-08 18:09:40 +0700

    init chapter001

---
+++ Chapter02/download_data.py
@@ -0,0 +1,36 @@
+import os
+import tarfile
+import pandas as pd
+from six.moves import urllib
+import matplotlib.pyplot as plt
+
+# CONFIG
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, 'housing.csv')
+    return pd.read_csv(csv_path)
+
+
+if __name__ == '__main__':
+    # fetch_data()
+    housing = load_housing_data()
+    # housing.head()
+    # housing.info()
+    housing.describe()
+    housing.hist(bins=50, figsize=(20, 15))
+    plt.show()
